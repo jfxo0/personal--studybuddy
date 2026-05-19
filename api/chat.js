44aa2fdf1c6fd68b6bb2c622b1c35f5b409d322d
jfxo0 <janisha.ly.wong@gmail.com>
2026-05-19 23:06:53 +0200

    ik hoop goede path

--- api/chat.js
+++ api/chat.js
@@ -1,4 +1,4 @@
-import { callAgent } from "./agent.js";
+import { callAgent } from "../agent.js";
 
 export default async function handler(req, res) {
     if (req.method !== "POST") {
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 
 import express, { response } from "express"
 import { ask } from "./load.js"
-import { callAgent } from "./agent.js"
+import { callAgent } from "./api"
 import multer from "multer";
 import { PDFLoader } from "@langchain/community/document_loaders/fs/pdf";
 import { RecursiveCharacterTextSplitter } from "@langchain/textsplitters";
