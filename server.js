b1db5884d4b71707f52c5396220d4157d8117809
jfxo0 <janisha.ly.wong@gmail.com>
2026-05-19 23:50:34 +0200

    fix server import

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 
 import express, { response } from "express"
-import { ask } from "./load.js"
 import { callAgent } from "./agent.js"
 import multer from "multer";
 import { PDFLoader } from "@langchain/community/document_loaders/fs/pdf";
